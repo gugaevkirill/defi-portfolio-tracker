eb81112312d9720f161a1187e5f0a89d6c5cd54a
kirill <gugaevkirill@gmail.com>
2023-05-06 02:33:09 +0400

    Fix tailwind styles

--- modules/wallets.tsx
+++ modules/wallets.tsx
@@ -7,7 +7,7 @@ const WalletStats: FC<Props> = () => {
   const [walletAddress, setWalletAddress] = useState('');
   const [addresses, setAddresses] = useState<string[]>([]);
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const addWallet = (e: React.FormEvent) => {
     e.preventDefault();
     if (walletAddress.trim()) {
       setAddresses([...addresses, walletAddress]);
@@ -15,42 +15,13 @@ const WalletStats: FC<Props> = () => {
     }
   };
 
-  // const [showAdd, setShowAdd] = useState(false);
-  // const toggleShowAdd = useCallback(() => setShowAdd((prevState) => !prevState), []);
-  //
-  // const { data, loading, refetch } = useQuery<getAudiences, getAudiencesVariables>(GET_AUDIENCES, {
-  //   variables: {
-  //     where: {
-  //       enabled: {
-  //         _eq: true,
-  //       },
-  //     },
-  //   },
-  //   fetchPolicy: 'cache-and-network',
-  // });
-
-  // const handleClose = useCallback(() => {
-  //   setShowAdd(false);
-  //   refetch();
-  // }, [refetch]);
-  //
-  // const handleSuccess = useCallback(() => {
-  //   refetch();
-  // }, [refetch]);
-  //
-  // const items = useMemo(
-  //   () => data?.audiences.filter((el) => !search || el.name.toLowerCase().includes(search.toString())),
-  //   [data, search],
-  // );
-
   return (
     <>
       <div className="mt-6 sm:mt-10 text-sm">
         <form
-          className="mt-6 flex max-w-md gap-x-4"
-          onSubmit={handleSubmit}
+          className="mt-6 max-w-md gap-x-4 mx-auto text-center"
+          onSubmit={addWallet}
         >
-          <label htmlFor="email-address" className="sr-only">Email address</label>
           <input
             type="text"
             value={walletAddress}
@@ -58,22 +29,21 @@ const WalletStats: FC<Props> = () => {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Enter wallet address"
           />
+
+          <ul role="list" className="mt-5">
+            {addresses.map((address, index) => (
+              <li key={index} className="justify-between py-2">
+                {address}
+              </li>
+            ))}
+          </ul>
+
           <button
-            type="submit"
-            className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
+            className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg my-5"
           >
             Compute
           </button>
         </form>
-      <div className="mt-6">
-        <ul role="list" className="divide-y">
-        {addresses.map((address, index) => (
-          <li key={index} className="justify-between py-2">
-            {address}
-          </li>
-        ))}
-        </ul>
-      </div>
       </div>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import WalletStats from "../modules/wallets";
 
 
 const MainPage = () => (
-  <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-    <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
-      <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+  <main className="p-24 min-h-screen bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100">
+    <div className="max-w-5xl mx-auto pt-5 sm:pt-24 lg:pt-32">
+      <h1 className="font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
         Portfolio Value from Debank
       </h1>
-      <p className="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
+      <p className="mt-6 text-lg text-slate text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
         Combine <a href="https://debank.com/">Debank.com</a> stats for multiple wallets
       </p>
       <WalletStats/>
