8a951d2bc5d3ec4eb458bfd3f8446358844eaf9c
kirill <gugaevkirill@gmail.com>
2023-05-06 01:29:19 +0400

    Change folders structure

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,3 @@
-import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <>
+      <main>{children}</main>
+    </>
+  );
 }
